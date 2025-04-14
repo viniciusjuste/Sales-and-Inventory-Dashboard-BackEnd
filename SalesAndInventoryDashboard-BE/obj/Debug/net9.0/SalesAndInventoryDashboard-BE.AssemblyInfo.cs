7a2dc5613ba451e3efe344060fe4a860dba6a276
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-04-14 20:09:18 -0300

    feat: criei a pasta e os arquivos onde vão ficar as lógicas da API

--- SalesAndInventoryDashboard-BE/obj/Debug/net9.0/SalesAndInventoryDashboard-BE.AssemblyInfo.cs
+++ SalesAndInventoryDashboard-BE/obj/Debug/net9.0/SalesAndInventoryDashboard-BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesAndInventoryDashboard-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6c90a86828e4dde7ad7d277fd63261d092dda9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710b91fdb21b9eaa97fb84cb1866148fe124e1e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesAndInventoryDashboard-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesAndInventoryDashboard-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
